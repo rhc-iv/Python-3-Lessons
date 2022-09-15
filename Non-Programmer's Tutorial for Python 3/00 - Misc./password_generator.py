24a3d7842d7e3020ab72388262383277cca2c02a
Robert Carr <86175637+rhc-iv@users.noreply.github.com>
2022-09-15 04:31:21 -0500

    Formatted some files in PyCharm

--- Non-Programmer's Tutorial for Python 3/00 - Misc./password_generator.py
+++ Non-Programmer's Tutorial for Python 3/00 - Misc./password_generator.py
@@ -2,6 +2,7 @@
 
 import string
 from random import *
-characters = string.ascii_letters + string.punctuation  + string.digits
-password =  "".join(choice(characters) for x in range(randint(8, 16)))
-print (password)
+
+characters = string.ascii_letters + string.punctuation + string.digits
+password = "".join(choice(characters) for x in range(randint(8, 16)))
+print(password)
--- Non-Programmer's Tutorial for Python 3/02 - Who Goes There/wgt_ex.py
+++ Non-Programmer's Tutorial for Python 3/02 - Who Goes There/wgt_ex.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-# Chapter exercise - Write a program that gets 2 string variables and 2 number variables from the user, concatenates (joins them together with no spaces) and displays the strings, then multiplies the two numbers on a new line.
+# Chapter exercise - Write a program that gets 2 string variables
+# and 2 number variables from the user, concatenates (joins them
+# together with no spaces) and displays the strings, then multiplies
+# the two numbers on a new line.
 
 string1 = input('String 1: ')
 string2 = input('String 2: ')
