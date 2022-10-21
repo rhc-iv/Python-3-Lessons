c4b43c2fbb4faab710d78069302c493ac15bfcea
Robert Carr <86175637+rhc-iv@users.noreply.github.com>
2022-10-21 01:41:37 -0500

    Formatted some files in PyCharm

--- Non-Programmer's Tutorial for Python 3/02 - Who Goes There/temperature.py
+++ Non-Programmer's Tutorial for Python 3/02 - Who Goes There/temperature.py
@@ -3,5 +3,5 @@
 # This program converts Fahrenheit to Celsius
 
 fahr_temp = float(input("Fahrenheit temperature: "))
-celc_temp = (fahr_temp - 32.0) *( 5.0 / 9.0)
-print("Celsius temperature:", celc_temp)
+calc_temp = (fahr_temp - 32.0) * (5.0 / 9.0)
+print("Celsius temperature:", calc_temp)
